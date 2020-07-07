bfd7752ddc5a8e2d99e49a83138e0785df46d00f
ArunGohil <arun.r@hungama.com>
2020-07-07 18:56:40 +0530

    added save method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -20,6 +20,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -45,6 +46,10 @@ class App extends React.Component {
     this.setState({ playlistName: name })
   }
 
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
   render() {
     return (
       <div>
@@ -58,7 +63,8 @@ class App extends React.Component {
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
-              onNameChange={this.updatePlaylistName} />
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist} />
           </div>
         </div>
       </div >
